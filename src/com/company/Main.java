75bafc37479c568ed6bdc8ff04f8622edee9c3d2
micah3252625 <micahmustaham@gmail.com>
2021-07-08 20:47:43 +0800

    Thresholding and Shrinking completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -32,7 +32,28 @@ public class Main {
     public static int getBlue(int pixel) {
         return pixel & 255;
     }
-    public static void Shrink(int threshold, String filename) {
+    public static int setPixel(int val) { return ((val << 16) | (val << 8) | val); }
+    public static BufferedImage getGrayScaleImg(BufferedImage img) { // method that gets that process the grayscale image
+
+        // initialize new Buffered Image
+        BufferedImage grayImg = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        // get the pixel of image and process it to grayscale
+        for (int w = 0; w < img.getWidth(); w++) {
+            for (int h = 0; h < img.getHeight(); h++) {
+                int grayPixel = img.getRGB(w, h);
+                int R = getRed(grayPixel) ;
+                int G = getGreen(grayPixel) ;
+                int B = getBlue(grayPixel);
+                int avg = (R + G + B) / 3;
+                grayPixel = (avg << 16) | (avg << 8) | avg;
+                grayImg.setRGB(w, h, grayPixel);
+            }
+        }
+        return grayImg; // return grayscale image
+    }
+
+    public static void Shrink(final int T) {
 
         // read or load image
         BufferedImage img = null;
@@ -42,26 +63,50 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        BufferedImage grayImg = getGrayScaleImg(img);
         // get image dimension
         int width = img.getWidth();
         int height = img.getHeight();
-        BufferedImage outputImg = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+        // conversion of grayscale image to binary image
+        BufferedImage binaryImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        int [][]Binary = new int[width][height];
 
-        for (int x = 0; x < width ; x++) {
+        for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                int pixel = img.getRGB(x, y);
-                int R = getRed(pixel) ;
-                int G = getGreen(pixel) ;
-                int B = getBlue(pixel);
-                int avg = (R + G + B) / 3;
-                outputImg.setRGB(x, y, (avg > threshold ? Color.WHITE.getRGB() : Color.BLACK.getRGB()));
+                Binary[x][y] = (grayImg.getRGB(x, y) & 255) > T ? 0 : 1;
+                binaryImg.setRGB(x, y, (Binary[x][y] == 0 ? setPixel(255) : setPixel(0)));
             }
         }
 
+        BufferedImage shrunkImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        int []neighborhood = new int[8];
+
+        for (int x = 1; x < width - 1; x++) {
+            for (int y = 1; y < height - 1; y++) {
+                int a0 = (binaryImg.getRGB(x, y) & 255) == 0 ? 1 : 0;
+                neighborhood[0] = (binaryImg.getRGB(x + 1, y) & 255) == 0 ? 1 : 0;
+                neighborhood[1] = (binaryImg.getRGB(x - 1, y) & 255) == 0 ? 1 : 0;
+                neighborhood[2] = (binaryImg.getRGB(x, y + 1) & 255) == 0 ? 1 : 0;
+                neighborhood[3] = (binaryImg.getRGB(x, y - 1) & 255) == 0 ? 1 : 0;
+                neighborhood[4] = (binaryImg.getRGB(x + 1, y + 1) & 255) == 0 ? 1 : 0;
+                neighborhood[5] = (binaryImg.getRGB(x + 1, y - 1) & 255) == 0 ? 1 : 0;
+                neighborhood[6] = (binaryImg.getRGB(x - 1, y + 1) & 255) == 0 ? 1 : 0;
+                neighborhood[7] = (binaryImg.getRGB(x - 1, y - 1) & 255) == 0 ? 1 : 0;
+                int sigma = 0;
+                for (int i = 0; i < neighborhood.length; i++) {
+                    sigma += neighborhood[i];
+                }
+                shrunkImg.setRGB(x, y, (sigma < 8 ? setPixel(255) : a0 == 0 ? setPixel(255) : setPixel(0)));
+            }
+        }
+
+
+
+
         // write image
         try {
-            WriteImage(outputImg, filename);
+            WriteImage(binaryImg, "threshold_102");
+            WriteImage(shrunkImg, "shrunk");
             System.out.println("Success!");
         } catch (IOException e) {
             System.out.println("Unable to process image!");
@@ -70,6 +115,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Shrink(102, "binary");
+        Shrink(102);
     }
 }
